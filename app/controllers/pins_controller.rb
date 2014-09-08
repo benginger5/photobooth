cc72f1112c1a6967f805f0bc0fdf1a28d1893658
Ben Ginger <benginger5@gmail.com>
2014-09-08 17:06:12 -0400

    Load pins in index action in reverse chronological order

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,24 +1,24 @@
 class PinsController < ApplicationController
   before_action :set_pin, only: [:show, :edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index]
 
   def index
-    @pins = Pin.all
+    @pins = Pin.all.order("created_at DESC")
   end
 
   def show
   end
 
   def new
-    @pin = current_user.pins.build
+    @pin = current_user.pins.build #set to current user
   end
 
   def edit
   end
 
   def create
-    @pin =  current_user.pins.build(pin_params)
+    @pin = current_user.pins.build(pin_params)
     if @pin.save
       redirect_to @pin, notice: 'Pin was successfully created.'
     else
@@ -45,14 +45,13 @@ class PinsController < ApplicationController
       @pin = Pin.find(params[:id])
     end
 
-  def correct_user
-    @pin = current_user.pins.find_by(id: params[:id])
-    redirect_to pins_path, notice: "Not Authorized to edit this pin" if @pin.nil?
-  end
+    def correct_user
+      @pin = current_user.pins.find_by(id: params[:id])
+      redirect_to pins_path, notice: "Not authorized to edit this pin" if @pin.nil?
+    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.    
-    
+    # Never trust parameters from the scary internet, only allow the white list through.
     def pin_params
       params.require(:pin).permit(:description, :image)
     end
-  end
+end
